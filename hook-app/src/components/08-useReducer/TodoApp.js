ca383484411057aa88cd2ba33aef4e55768faaa0
José Rafael Morente González <josemorenteg98@gmail.com>
2022-01-07 21:14:13 -0600

    [UPDATED] TodoApp.js
    Agregado de TODOS en localstorage.

--- hook-app/src/components/08-useReducer/TodoApp.js
+++ hook-app/src/components/08-useReducer/TodoApp.js
@@ -1,31 +1,96 @@
-import React, { useReducer } from 'react'
+import React, { useEffect, useReducer } from 'react'
 import './style.css'
 import { todoReducer } from './todoReducer'
+import { useForm } from '../../hooks/useForm'
 
-const initialState = [
-    {
-        id: new Date().getTime(),
-        desc: 'Aprender React',
-        done: false
-    }
-]
+const init = () => {
+    return JSON.parse(localStorage.getItem('todos')) || [];
+    // return [{
+    //     id: new Date().getTime(),
+    //     desc: 'Aprender React',
+    //     done: false
+    // }]
+}
 
 export const TodoApp = () => {
 
 
-    const [todos] = useReducer(todoReducer, initialState)
+    const [todos, dispatch] = useReducer(todoReducer, [], init)
 
-    console.log(todos)
+    const { values, handleInputChange, reset } = useForm({
+        descripcion: ''
+    })
+
+    console.log(values)
+
+    useEffect(() => {
+        localStorage.setItem('todos', JSON.stringify(todos))
+    }, [todos])
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if (values.descripcion.trim().length <= 1) {
+            return;
+        }
+
+        const newTodo = {
+            id: new Date().getTime(),
+            desc: values.descripcion,
+            done: false
+        }
+
+        const action = {
+            type: 'add',
+            payload: newTodo
+        }
+        dispatch(action);
+        reset();
+        console.log('Nueva tarea')
+    }
+
     return (
         <>
-            <h1>Todo App</h1>
+            <h1>Todo App ( {todos.length} )</h1>
             <hr />
 
-            <ul>
-                <li>Hola</li>
-                <li>Hola Goku</li>
-                <li>Hola de Nuevo</li>
-            </ul>
+            <div className='row'>
+                <div className='col-7'>
+                    <ul className='list-group list-group-flush'>
+                        {
+                            todos.map((todo, i) => (
+                                <li key={todo.id}
+                                    className='list-group-item'>
+                                    <p className='text-center'>
+                                        {i + 1}. {todo.desc}
+                                    </p>
+                                    <button className='btn btn-danger'>
+                                        Borrar
+                                    </button>
+                                </li>
+                            ))
+                        }
+                    </ul>
+                </div>
+                <div className='col-5'>
+                    <h4>Agregar TODO</h4>
+                    <hr />
+
+                    <form onSubmit={handleSubmit}>
+                        <input type='text' name='descripcion'
+                            placeholder='Aprender' autoComplete='off'
+                            className='form-control'
+                            value={values.descripcion}
+                            onChange={handleInputChange}>
+                        </input>
+                        <div className="d-grid gap-2">
+                            <button className='btn btn-outline-primary mt-1' type='submit'>
+                                Agregar
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </>
     )
 }
