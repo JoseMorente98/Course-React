2ce43badb671b92b69fdc1b2a6ff13e5932986b0
José Rafael Morente González <josemorenteg98@gmail.com>
2022-05-03 08:15:03 -0600

    [ADDED] useForm.js

--- heroes-app/src/components/search/SearchScreen.js
+++ heroes-app/src/components/search/SearchScreen.js
@@ -1,9 +1,47 @@
 import React from 'react'
+import { useForm } from '../../hooks/useForm';
 
 export const SearchScreen = () => {
+
+    const { values, handleInputChange } = useForm({
+        search: '',
+    })
+
+    const { search } = values;
+
+    const handleSearch = (e) => {
+        e.preventDefault()
+        console.log(search)
+    }
+
     return (
-        <div>
-            <h1>Search Screen</h1>
-        </div>
+        <>
+            <h1>Búsqueda</h1>
+            <hr />
+
+            <div className='row'>
+                <div className='col-5'>
+                    <h4>Buscar</h4>
+                    <hr />
+
+                    <form onSubmit={handleSearch}>
+                        <div className="form-group">
+                            <input type='text'
+                                placeholder='Buscar héroe'
+                                className='form-control'
+                                name="search"
+                                autoComplete='off'
+                                value={search}
+                                onChange={handleInputChange}
+                            />
+                        </div>
+                        <button className='btn btn-outline-dark mt-1'
+                            type='submit'>
+                            Buscar
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </>
     )
 }
